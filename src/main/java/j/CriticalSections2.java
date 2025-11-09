38682074fd3ac73170467448c323496f0da06e76
Anna <melana0607061@gmail.com>
2025-11-09 13:32:18 +0300

    Critical section synchronized with lock

---
+++ src/main/java/j/CriticalSections2.java
@@ -0,0 +1,60 @@
+package j;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CriticalSections2 {
+    int i;
+    public void f(){
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            i++;
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i);
+            }
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void g(){
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            i--;
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i);
+            }
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void t(){
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            i+=10;
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i);
+            }
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args){
+        CriticalSections cs = new CriticalSections();
+        new Thread(){
+            public void run(){
+                cs.f();
+            }
+        }.start();
+        new Thread(){
+            public void run(){
+                cs.g();
+            }
+        }.start();
+        cs.t();
+    }
+}
+
