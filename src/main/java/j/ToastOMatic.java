6e5979db03bc4ada2b2ff2a2597e4016cbbb1603
Anna <melana0607061@gmail.com>
2025-11-18 21:59:52 +0300

    Toasts working

--- src/main/java/j/ToastOMatic.java
+++ src/main/java/j/ToastOMatic.java
@@ -6,41 +6,58 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-class Toast{
-    public enum Status{DRY, PEANUT_BUTTER, JELLY, SANDWICH}
-    private Status status= Status.DRY;
+class Toast {
+    public enum Status {DRY, PEANUT_BUTTER, JELLY, SANDWICH}
+
+    private Status status = Status.DRY;
     private final int id;
-    public Toast(int idn){id=idn;}
-    public void peanutButter(){
-        status=Status.PEANUT_BUTTER;
+
+    public Toast(int idn) {
+        id = idn;
     }
-    public void jelly(){
-        status=Status.JELLY;
+
+    public void peanutButter() {
+        status = Status.PEANUT_BUTTER;
     }
-    public void sandwich(){
-        status=Status.SANDWICH;
+
+    public void jelly() {
+        status = Status.JELLY;
     }
-    public Status getStatus() { return status;}
-    public int getId() { return id;}
-    public String toString(){
-        return "Toast"+id+": "+status;
+
+    public void sandwich() {
+        status = Status.SANDWICH;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+        return "Toast" + id + ": " + status;
     }
 }
 
-class ToastQueue extends LinkedBlockingQueue<Toast>{}
+class ToastQueue extends LinkedBlockingQueue<Toast> {
+}
 
-class Toaster implements Runnable{
+class Toaster implements Runnable {
     private ToastQueue toasts;
-    private int count=0;
+    private int count = 0;
     private Random rand = new Random();
-    public Toaster(ToastQueue tq){
-        toasts=tq;
+
+    public Toaster(ToastQueue tq) {
+        toasts = tq;
     }
+
     @Override
     public void run() {
         try {
-            while (!Thread.interrupted()){
-                TimeUnit.MICROSECONDS.sleep(100+rand.nextInt(500));
+            while (!Thread.interrupted()) {
+                TimeUnit.MICROSECONDS.sleep(100 + rand.nextInt(500));
                 Toast t = new Toast(count++);
                 System.out.println(t);
                 toasts.put(t);
@@ -52,24 +69,21 @@ class Toaster implements Runnable{
     }
 }
 
-class Butterer implements Runnable{
-    Sandwich sandwich;
+class Butterer implements Runnable {
     private ToastQueue dryToasts, butteredToast;
-    public Butterer(ToastQueue dryToasts, ToastQueue butteredToast, Sandwich s){
-        sandwich=s;
-        this.dryToasts=dryToasts;
-        this.butteredToast=butteredToast;
+
+    public Butterer(ToastQueue dryToasts, ToastQueue butteredToast) {
+        this.dryToasts = dryToasts;
+        this.butteredToast = butteredToast;
     }
-    public void run(){
+
+    public void run() {
         try {
-            while (!Thread.interrupted()){
+            while (!Thread.interrupted()) {
                 Toast t = dryToasts.take();
                 t.peanutButter();
                 System.out.println(t);
                 butteredToast.put(t);
-                synchronized (sandwich){
-                    notifyAll();
-                }
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
@@ -77,24 +91,21 @@ class Butterer implements Runnable{
     }
 }
 
-class Jeller implements Runnable{
-    Sandwich sandwich;
+class Jeller implements Runnable {
     private ToastQueue dryToasts, jelledToast;
-    public Jeller(ToastQueue dryToasts, ToastQueue jelledToast, Sandwich s){
-        sandwich=s;
-        this.dryToasts=dryToasts;
-        this.jelledToast=jelledToast;
+
+    public Jeller(ToastQueue dryToasts, ToastQueue jelledToast) {
+        this.dryToasts = dryToasts;
+        this.jelledToast = jelledToast;
     }
-    public void run(){
+
+    public void run() {
         try {
-            while (!Thread.interrupted()){
+            while (!Thread.interrupted()) {
                 Toast t = dryToasts.take();
                 t.jelly();
                 System.out.println(t);
                 jelledToast.put(t);
-                synchronized (sandwich){
-                    notifyAll();
-                }
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
@@ -102,17 +113,17 @@ class Jeller implements Runnable{
     }
 }
 
-class ToastButterJelly extends Toast{
-    Sandwich sandwich;
+class ToastButterJelly extends Toast {
     private Toast butter;
     private Toast jelly;
-    public ToastButterJelly(Toast butter, Toast jelly, Sandwich s){
+
+    public ToastButterJelly(Toast butter, Toast jelly) {
         super(butter.getId());
-        sandwich=s;
-        this.butter=butter;
-        this.jelly=jelly;
+        this.butter = butter;
+        this.jelly = jelly;
     }
-    public synchronized boolean isButtered(){
+
+    public synchronized boolean isButtered() {
         while (!Thread.interrupted()) {
             if (butter != null) {
                 return true;
@@ -120,7 +131,8 @@ class ToastButterJelly extends Toast{
         }
         return false;
     }
-    public synchronized boolean isJelled(){
+
+    public synchronized boolean isJelled() {
         while (!Thread.interrupted()) {
             if (jelly != null) {
                 return true;
@@ -131,31 +143,28 @@ class ToastButterJelly extends Toast{
 
     @Override
     public String toString() {
-        return "Sandwich "+super.getId();
+        return "Sandwich " + super.getId();
     }
 }
 
-class Sandwich implements Runnable{
+class Sandwich implements Runnable {
     private ToastQueue butteredToast, jelledToast, finishedToast;
-    public Sandwich(ToastQueue butteredToast, ToastQueue jelledToast, ToastQueue finishedToast){
-        this.butteredToast=butteredToast;
-        this.jelledToast=jelledToast;
-        this.finishedToast=finishedToast;
+
+    public Sandwich(ToastQueue butteredToast, ToastQueue jelledToast, ToastQueue finishedToast) {
+        this.butteredToast = butteredToast;
+        this.jelledToast = jelledToast;
+        this.finishedToast = finishedToast;
     }
-    public void run(){
+
+    public void run() {
         try {
-            while (!Thread.interrupted()){
+            while (!Thread.interrupted()) {
                 Toast t = butteredToast.take();
-                Toast t2=jelledToast.take();
-                ToastButterJelly toast = new ToastButterJelly(t, t2, this);
-                synchronized (this) {
-                    while (!toast.isButtered() || !toast.isJelled()) {
-                        wait();
-                    }
-                    toast.sandwich();
-                    System.out.println("sandwich with peanut butter and jelly " + t2.getId());
-                    finishedToast.put(toast);
-                }
+                Toast t2 = jelledToast.take();
+                ToastButterJelly toast = new ToastButterJelly(t, t2);
+                toast.sandwich();
+                System.out.println("sandwich with peanut butter "+t.getId() +" and jelly " + t2.getId());
+                finishedToast.put(toast);
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
@@ -163,36 +172,38 @@ class Sandwich implements Runnable{
     }
 }
 
-class Eater implements Runnable{
+class Eater implements Runnable {
     private ToastQueue finishedQueue;
     private int counter = 0;
+
     public Eater(ToastQueue finished) {
         finishedQueue = finished;
     }
+
     public void run() {
         try {
             while (!Thread.interrupted()) {
                 Toast t = finishedQueue.take();
-                System.out.println("Chomp "+t);
+                System.out.println("Chomp " + counter++);
             }
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
         }
     }
 }
+
 public class ToastOMatic {
     public static void main(String[] args) throws InterruptedException {
         ToastQueue
-                dryQueue= new ToastQueue(),
+                dryQueue = new ToastQueue(),
                 butteredToast = new ToastQueue(),
                 jelledToast = new ToastQueue(),
                 finishedQueue = new ToastQueue();
         ExecutorService exec = Executors.newCachedThreadPool();
-        Sandwich s;
         exec.execute(new Toaster(dryQueue));
-        exec.execute(s=new Sandwich(butteredToast, jelledToast, finishedQueue));
-        exec.execute(new Butterer(dryQueue,butteredToast,s));
-        exec.execute(new Jeller(dryQueue, jelledToast,s));
+        exec.execute(new Sandwich(butteredToast, jelledToast, finishedQueue));
+        exec.execute(new Butterer(dryQueue, butteredToast));
+        exec.execute(new Jeller(dryQueue, jelledToast));
         exec.execute(new Eater(finishedQueue));
         TimeUnit.SECONDS.sleep(1);
         exec.shutdownNow();
